2b7ae266c9006f4ced3c95e45ae42001fb732e8b
Marko Pohl <pohl@machbar.eu>
2014-01-28 11:17:21 +0100

    move tmpl to fluid

--- fileadmin/templates/typoscript/typo3-test.setup.ts
+++ fileadmin/templates/typoscript/typo3-test.setup.ts
@@ -84,7 +84,7 @@ page {
 	10 = FLUIDTEMPLATE
     10 {
         template = FILE
-        template.file = fileadmin/templates/page.html
+        template.file = fileadmin/templates/fluid/page.html
         variables {
             content < styles.content.get
             navigation  < lib.field_navigation
